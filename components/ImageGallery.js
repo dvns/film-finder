df2eaa6cc758b5fb0cff1189cfefd1518124aa3f
Davin Suen <y6suen@gmail.com>
2020-12-25 20:14:35 -0500

    reorganize flickr query params into variables

--- components/ImageGallery.js
+++ components/ImageGallery.js
@@ -1,10 +1,21 @@
 import { useState, useEffect } from 'react';
 
 const API_KEY = process.env.NEXT_PUBLIC_FLICKR_API_KEY;
+const sort = 'interestingness-desc';
+const perPage = 50;
+const getDate = (months) => {
+  let d = new Date();
+  d.setMonth(d.getMonth() - months);
+  return d.getTime() / 1000;
+};
+const minDate = getDate(12);
+const maxDate = getDate(1)
+const extras = 'url_z'
+
 
 export default function ImageGallery({ queryString }) {
   const [images, setImages] = useState([]);
-  const apiUrl = `https://api.flickr.com/services/rest/?method=flickr.photos.search&api_key=${API_KEY}&text=${queryString.replace(/\s/g, '+')}&sort=relevance&extras=url_z&per_page=25&content_type=1&format=json&nojsoncallback=1`;
+  const apiUrl = `https://api.flickr.com/services/rest/?method=flickr.photos.search&api_key=${API_KEY}&text=${queryString.replace(/\s/g, '+')}&sort=${sort}&extras=${extras}&min_upload_date=${minDate}&max_upload_date=${maxDate}&per_page=${perPage}&content_type=1&format=json&nojsoncallback=1`;
   
 
   useEffect(() => {
